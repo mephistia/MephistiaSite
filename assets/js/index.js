cff68fdbc53b900d6149306cb1229aaf16e0e88f
Julia Stefanello <mephistia.jcs@outlook.com>
2021-02-26 20:29:23 -0300

    Fix active link

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,11 +1,12 @@
 export function activeLink(){
     const menu = document.querySelector('nav');
     const path = window.location.pathname.split('/').pop();
+    menu.querySelector(`.home-link`).classList.add('active-link');
+
     if (path.length > 0){
         menu.querySelector(`a[href="${path}"]`).classList.add('active-link');
-    }
-    else {
-        menu.querySelector(`a[href="/]`).classList.add('active-link');
+        menu.querySelector(`.home-link`).classList.remove('active-link');
+
     }
 }
 
